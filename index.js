b6a44c0391d32d33ee217d003dfa571a2fd62925
= <=>
2023-05-19 07:39:34 -0500

    Actividad en Clase - AppPractica

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 // console.log(express.raw.PORT);
-const port = process.env.PORT || 8083
+const PORT = process.env.PORT || 8083
 
 app.get('/productosAlimenticios', (req, res) => {
     res.json({mensaje: "Servidor contestando a petición GET a la ruta de productosAlimenticios (Rogelio Zamarripa Treviño - 18100248)"});
@@ -12,7 +12,7 @@ app.post('/proveedorProducto', (req, res) => {
     res.json({mensaje: "Servidor contestando a petición POST a la ruta de proveedorProducto (Rogelio Zamarripa Treviño - 18100248)"});
 });
 
-app.listen(port, ()=>
+app.listen(PORT, ()=>
 {
-    console.log(`Servidor Express corriendo y escuchando en el puerto ${port}`)
+    console.log(`Servidor Express corriendo y escuchando en el puerto ${PORT}`)
 })
